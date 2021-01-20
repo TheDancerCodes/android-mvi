8be6a0e0db10391c3ae66f6fb127038ee5c2efc2
Roger Taracha <rojtaracha@gmail.com>
2021-01-20 15:16:09 +0300

    Rendering the View 1 (#3)
    * AllCreaturesActivity - Add a TAG for logging in the companion object
    
    * Make the activity an MVI view, typed by the corresponding intent type and ViewState type.
    
    * Implement the MVIView methods
    
    * loadIntent()  - Function to create a Load Intent Observable
    
    * clearAllCreaturesPublisher() - A PublishSubject that emits a clear all intent when a user taps clear all in the menu.
    
    * disposables() - A CompositeDisposable to dispose of subscriptions in our activities
    
    * clearIntent() - private function to return a clear all event observable which just returns the corresponding published subject.
    
    * app/Injection - Set up ViewModel in the Activity. This is an injection object we will use to perform injection of the dependencies that our processor holders will need in their constructors.
    
    * util/CreaturemonViewModelFactory - can be used to create an AllCreaturesViewModel by passing in an AllCreaturesProcessorHolder.
    
    * AllCreaturesActivity - Add a ViewModel property using by lazy, the ViewModelProviders class and ViewModelFactory.
      * Override onStart() - to bind the ViewModel and onStop() - to clear our Composite Disposable
    
    * Update the onOptionsItemSelected() to publish a ClearAllCreaturesIntent when a user selects clear all in the menu

---
+++ app/src/main/java/com/raywenderlich/android/creaturemon/util/CreaturemonViewModelFactory.kt
@@ -0,0 +1,31 @@
+package com.raywenderlich.android.creaturemon.util
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.raywenderlich.android.creaturemon.allcreatures.AllCreaturesProcessorHolder
+import com.raywenderlich.android.creaturemon.allcreatures.AllCreaturesViewModel
+import com.raywenderlich.android.creaturemon.app.Injection
+
+/**
+ * CreaturemonViewModelFactory - used to create an AllCreaturesViewModel by passing in
+ * an AllCreaturesProcessorHolder.
+ */
+class CreaturemonViewModelFactory private constructor(
+        private val applicationContext: Context
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass == AllCreaturesViewModel::class.java) {
+            return AllCreaturesViewModel(
+                    AllCreaturesProcessorHolder(
+                            Injection.provideCreatureRepository(applicationContext),
+                            Injection.provideSchedulerProvider())) as T
+        }
+        throw IllegalAccessException("Unknown model class $modelClass")
+    }
+
+    companion object : SingletonHolderSingleArg<CreaturemonViewModelFactory, Context>
+        (::CreaturemonViewModelFactory)
+}
