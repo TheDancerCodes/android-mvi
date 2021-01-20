3cfab5d463bd28964b2fffe98bfd07304a49aa56
Roger Taracha <rojtaracha@gmail.com>
2021-01-20 16:54:24 +0300

    Rendering the View 2 (#4)
    * Implementing the MVIView interface
    
    * Add a ViewModel property to the Activity
    
    * Add the necessary RxJava code
    
    * Update CreaturemonViewModelFactory to allow it to create AddCreatureViewModel instances
    
    * CreatureActivity - Add a TAG for error logging in the companion object.
    
    * Add PublishSubjects that emit when all the intents on the screen are expressed
    
    * Add a composite disposable for our subscriptions
    
    * Add ViewModel property, instantiate it using by lazy and create it from a CreaturemonViewModelFactory
    
    * Use private functions to turn our publish subjects into Observables:
      * avatarIntent()
      * nameIntent()
      * intelligenceIntent()
      * strengthIntent()
      * enduranceIntent()
      * saveIntent()
    
    * Implement the MVIView functions - intent() & render()
    
    * Handle the user interacting with the view:
      * When the user selects items in the creature attributes spinners, such a picking an intelligence value, we'll emit those into the observable stream by calling onNext() on the associated PublishSubject, passing in the appropriate intent.
    
      * In the EditText onChangedListener(), we call onNext() on our name PublishSubject using the current text and the name edit text.
    
      * For taps on the saveButton, we publish a save intent.
    
      * When the user picks an avatar drawable, we publish an avatar intent.
    
    * Start building our the render() method:
    
      * Check to see if a save is complete, in which case we show a toast and finish the activity.
    
      * Add a ProgressBar to activity_creature.xml & land/activity_creature.xml to use in the processing state.
    
      * Handle the processing state by showing the progress bar and hiding the save button or the reverse if not processing.
    
      * If a drawable has been selected, we show the avatar image in the image view, save the chosen resource ID so we can pass it along when saving the creature and call hideTapLabel
    
      * Set the HitPointsText to the value of the hit points for the creature passed along in the state.
    
      * Handle the error case by showing a toast and logging the error.
    
    * Run the app
    
    * NOTE: Each interaction on the AddCreature screen is causing an Intent to be started in our MVI cycle, which becomes an Action, a Result and a new State to be rendered.

--- app/src/main/java/com/raywenderlich/android/creaturemon/addcreature/CreatureActivity.kt
+++ app/src/main/java/com/raywenderlich/android/creaturemon/addcreature/CreatureActivity.kt
@@ -34,19 +34,65 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
 import com.raywenderlich.android.creaturemon.R
 import com.raywenderlich.android.creaturemon.data.model.AttributeStore
 import com.raywenderlich.android.creaturemon.data.model.AttributeValue
 import com.raywenderlich.android.creaturemon.data.model.Avatar
 import com.raywenderlich.android.creaturemon.addcreature.avatars.AvatarAdapter
 import com.raywenderlich.android.creaturemon.addcreature.avatars.AvatarBottomDialogFragment
+import com.raywenderlich.android.creaturemon.mvibase.MviView
+import com.raywenderlich.android.creaturemon.util.CreaturemonViewModelFactory
+import com.raywenderlich.android.creaturemon.util.visible
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_creature.*
 
 
-class CreatureActivity : AppCompatActivity(), AvatarAdapter.AvatarListener {
+class CreatureActivity : AppCompatActivity(),
+        AvatarAdapter.AvatarListener,
+        MviView<AddCreatureIntent, AddCreatureViewState> {
+
+  /*
+   * PublishSubjects that emit when all the intents on the screen are expressed
+   */
+  private val avatarIntentPublisher =
+          PublishSubject.create<AddCreatureIntent.AvatarIntent>()
+
+  private val nameIntentPublisher =
+          PublishSubject.create<AddCreatureIntent.NameIntent>()
+
+  private val intelligenceIntentPublisher =
+          PublishSubject.create<AddCreatureIntent.IntelligenceIntent>()
+
+  private val strengthIntentPublisher =
+          PublishSubject.create<AddCreatureIntent.StrengthIntent>()
+
+  private val enduranceIntentPublisher =
+          PublishSubject.create<AddCreatureIntent.EnduranceIntent>()
+
+  private val saveIntentPublisher =
+          PublishSubject.create<AddCreatureIntent.SaveIntent>()
+
+  // Add a composite disposable for our subscriptions
+  private val disposables = CompositeDisposable()
+
+  // Property to hold the chosen resource ID for the avatar
+  private var avatarResourceId = 0 // TODO: save in ViewModel for a device rotation
+
+  // Add ViewModel property, instantiate it using by lazy and create it from a
+  // CreaturemonViewModelFactory
+  private val viewModel: AddCreatureViewModel by lazy(LazyThreadSafetyMode.NONE) {
+    ViewModelProviders
+            .of(this, CreaturemonViewModelFactory.getInstance(this))
+            .get(AddCreatureViewModel::class.java)
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -59,10 +105,117 @@ class CreatureActivity : AppCompatActivity(), AvatarAdapter.AvatarListener {
     configureClickListeners()
   }
 
+  override fun onStart() {
+    super.onStart()
+
+    // Bind the ViewModel
+    bind()
+  }
+
+  override fun onStop() {
+    super.onStop()
+
+    // Clear our Composite Disposable
+    disposables.clear()
+  }
+
+  /*
+   * Subscribe to the ViewModel states observable & tell the ViewModel to process our user intents,
+   * in which it subscribes to our intents observable.
+   *
+   * Pass the render function into the ViewModel state subscription as an onNext handler
+   */
+  private fun bind() {
+    disposables.add(viewModel.states().subscribe(this::render))
+    viewModel.processIntents(intents())
+  }
+
+  /*
+   * MVI View Functions
+   *
+   * intents() - provides the intents that the ViewModel will subscribe to.
+   * Implement the intents override by merging all of our intent observables.
+   *
+   * render() - used to render a new state to the screen.
+   */
+  override fun intents(): Observable<AddCreatureIntent> {
+    return Observable.merge(
+            avatarIntent(),
+            nameIntent(),
+            intelligenceIntent(),
+            strengthIntent())
+            .mergeWith(enduranceIntent())
+            .mergeWith(saveIntent())
+  }
+
+  override fun render(state: AddCreatureViewState) {
+
+    // Check to see if a save is complete, in which case we show a toast and finish the activity.
+    if (state.isSaveComplete) {
+      Toast.makeText(this, getString(R.string.creature_saved), Toast.LENGTH_SHORT).show()
+      finish()
+      return
+    }
+
+    if (state.isProcessing) {
+      progressBar.visible = true
+      saveButton.visible = false
+    } else {
+      progressBar.visible = false
+      saveButton.visible = true
+    }
+
+    //  If a drawable has been selected, we show the avatar image in the image view,
+    //  save the chosen resource ID so we can pass it along when saving the creature and
+    //  call hideTapLabel
+    if (state.isDrawableSelected) {
+      avatarImageView.setImageResource(state.creature.drawable)
+      avatarResourceId = state.creature.drawable
+      hideTapLabel()
+    }
+
+    // Set the HitPoints text to the value of the hit points for the creature passed along
+    // in the state.
+    hitPoints.text = state.creature.hitPoints.toString()
+
+    // Handle the error case by showing a toast and logging the error.
+    if (state.error != null) {
+      Toast.makeText(this, state.error.message, Toast.LENGTH_SHORT).show()
+      Log.e(TAG, "Error creating creature: ${state.error.message}")
+    }
+  }
+
+  /*
+     * Use private functions to turn our publish subjects into Observables
+     */
+  private fun avatarIntent(): Observable<AddCreatureIntent.AvatarIntent> {
+    return avatarIntentPublisher
+  }
+
+  private fun nameIntent(): Observable<AddCreatureIntent.NameIntent> {
+    return nameIntentPublisher
+  }
+
+  private fun intelligenceIntent(): Observable<AddCreatureIntent.IntelligenceIntent> {
+    return intelligenceIntentPublisher
+  }
+
+  private fun strengthIntent(): Observable<AddCreatureIntent.StrengthIntent> {
+    return strengthIntentPublisher
+  }
+
+  private fun enduranceIntent(): Observable<AddCreatureIntent.EnduranceIntent> {
+    return enduranceIntentPublisher
+  }
+
+  private fun saveIntent(): Observable<AddCreatureIntent.SaveIntent> {
+    return saveIntentPublisher
+  }
+
+
   private fun configureUI() {
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
     title = getString(R.string.add_creature)
-    // TODO: hide label
   }
 
   private fun configureSpinnerAdapters() {
@@ -74,33 +227,42 @@ class CreatureActivity : AppCompatActivity(), AvatarAdapter.AvatarListener {
         android.R.layout.simple_spinner_dropdown_item, AttributeStore.ENDURANCE)
   }
 
+  /*
+   * Handle the user interacting with the view.
+   *
+   * When the user selects items in the creature attributes spinners
+   */
   private fun configureSpinnerListeners() {
     intelligence.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
       override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        // TODO: handle selection
+        intelligenceIntentPublisher.onNext(AddCreatureIntent.IntelligenceIntent(position))
       }
       override fun onNothingSelected(parent: AdapterView<*>?) {}
     }
     strength.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
       override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        // TODO: handle selection
+        strengthIntentPublisher.onNext(AddCreatureIntent.StrengthIntent(position))
       }
       override fun onNothingSelected(parent: AdapterView<*>?) {}
     }
     endurance.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
       override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        // TODO: handle selection
+        enduranceIntentPublisher.onNext(AddCreatureIntent.EnduranceIntent(position))
       }
       override fun onNothingSelected(parent: AdapterView<*>?) {}
     }
   }
 
+  /*
+   * In the EditText onChangedListener(), we call onNext() on our name PublishSubject using the
+   * current text and the name edit text.
+   */
   private fun configureEditText() {
     nameEditText.addTextChangedListener(object : TextWatcher {
       override fun afterTextChanged(s: Editable?) {}
       override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
       override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        // TODO: handle text changed
+        nameIntentPublisher.onNext(AddCreatureIntent.NameIntent(s.toString()))
       }
     })
   }
@@ -111,17 +273,29 @@ class CreatureActivity : AppCompatActivity(), AvatarAdapter.AvatarListener {
       bottomDialogFragment.show(supportFragmentManager, "AvatarBottomDialogFragment")
     }
 
+    // For taps on the saveButton, we publish a save intent.
     saveButton.setOnClickListener {
-      // TODO: handle save button clicked
+      saveIntentPublisher.onNext(AddCreatureIntent.SaveIntent(
+              avatarResourceId,
+              nameEditText.text.toString(),
+              intelligence.selectedItemPosition,
+              strength.selectedItemPosition,
+              endurance.selectedItemPosition
+      ))
     }
   }
 
+  // When the user picks an avatar drawable, we publish an avatar intent.
   override fun avatarClicked(avatar: Avatar) {
-    // TODO: handle avatar clicked
+    avatarIntentPublisher.onNext(AddCreatureIntent.AvatarIntent(avatar.drawable))
     hideTapLabel()
   }
 
   private fun hideTapLabel() {
     tapLabel.visibility = View.INVISIBLE
+  }
+
+  companion object {
+    private const val TAG = "CreatureActivity"
   }
 }
--- app/src/main/java/com/raywenderlich/android/creaturemon/util/CreaturemonViewModelFactory.kt
+++ app/src/main/java/com/raywenderlich/android/creaturemon/util/CreaturemonViewModelFactory.kt
@@ -3,6 +3,8 @@ package com.raywenderlich.android.creaturemon.util
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.raywenderlich.android.creaturemon.addcreature.AddCreatureProcessorHolder
+import com.raywenderlich.android.creaturemon.addcreature.AddCreatureViewModel
 import com.raywenderlich.android.creaturemon.allcreatures.AllCreaturesProcessorHolder
 import com.raywenderlich.android.creaturemon.allcreatures.AllCreaturesViewModel
 import com.raywenderlich.android.creaturemon.app.Injection
@@ -23,6 +25,16 @@ class CreaturemonViewModelFactory private constructor(
                             Injection.provideCreatureRepository(applicationContext),
                             Injection.provideSchedulerProvider())) as T
         }
+
+        // Allow CreaturemonViewModelFactory to create AddCreatureViewModel instances
+        if (modelClass == AddCreatureViewModel::class.java) {
+            return AddCreatureViewModel(
+                    AddCreatureProcessorHolder(
+                            Injection.provideCreatureRepository(applicationContext),
+                            Injection.provideCreatureGenerator(),
+                            Injection.provideSchedulerProvider())) as T
+        }
+
         throw IllegalAccessException("Unknown model class $modelClass")
     }
 
